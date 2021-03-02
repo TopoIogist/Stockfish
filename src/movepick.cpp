3dc9f95e7f8cb7037f36bfa755941fc1ae4c5a1f
Topologist <thelisting@web.de>
2021-03-02 15:31:53 +0100

    Test: Use STL algorithms once again to sort moves.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -29,19 +29,11 @@ namespace {
     QSEARCH_TT, QCAPTURE_INIT, QCAPTURE, QCHECK_INIT, QCHECK
   };
 
-  // partial_insertion_sort() sorts moves in descending order up to and including
+  // partial_sort() sorts moves in descending order up to and including
   // a given limit. The order of moves smaller than the limit is left unspecified.
-  void partial_insertion_sort(ExtMove* begin, ExtMove* end, int limit) {
-
-    for (ExtMove *sortedEnd = begin, *p = begin + 1; p < end; ++p)
-        if (p->value >= limit)
-        {
-            ExtMove tmp = *p, *q;
-            *p = *++sortedEnd;
-            for (q = sortedEnd; q != begin && *(q - 1) < tmp; --q)
-                *q = *(q - 1);
-            *q = tmp;
-        }
+  void partial_sort(ExtMove* begin, ExtMove* end, int limit) {
+    auto const partitionEnd = std::stable_partition(begin, end, [limit](ExtMove const & mv){return mv.value >= limit;});
+    std::stable_sort(begin, partitionEnd, [](ExtMove const & a, ExtMove const & b) {return a.value > b.value;});
   }
 
 } // namespace
@@ -200,7 +192,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_sort(cur, endMoves, -3000 * depth);
       }
 
       ++stage;
