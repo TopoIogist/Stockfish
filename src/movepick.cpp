7650e2284f6c51eddea24db23581fdc96cd78a26
Topologist <thelisting@web.de>
2021-03-21 11:43:25 +0100

    Take 4 bench 4112661

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -34,20 +34,16 @@ namespace {
   // partial_insertion_sort() sorts moves in descending order up to and including
   // a given limit. The order of moves smaller than the limit is left unspecified.
   void partial_insertion_sort(ExtMove* begin, ExtMove* end) {
-    int limit;
     ExtMove* median_move = begin + (end-begin)/5;
     std::nth_element(begin, median_move, end,[](ExtMove const & a, ExtMove const & b) {return a.value > b.value;});
-    limit = median_move->value;
 
-    for (ExtMove *sortedEnd = begin, *p = begin + 1; p <= median_move; ++p)
-        if (p->value >= limit)
-        {
-            ExtMove tmp = *p, *q;
-            *p = *++sortedEnd;
-            for (q = sortedEnd; q != begin && *(q - 1) < tmp; --q)
-                *q = *(q - 1);
-            *q = tmp;
-        }
+    for (ExtMove *sortedEnd = begin, *p = begin + 1; p <= median_move; ++p)  {
+        ExtMove tmp = *p, *q;
+        *p = *++sortedEnd;
+        for (q = sortedEnd; q != begin && *(q - 1) < tmp; --q)
+            *q = *(q - 1);
+        *q = tmp;
+    }
   }
 
 } // namespace
