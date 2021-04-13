abe94af481504c9973d2698a4196ab4fb8e4db45
Topologist <thelisting@web.de>
2021-04-13 23:41:46 +0200

    Take 3 bench 4578583

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1091,7 +1091,7 @@ Value Eval::evaluate(const Position& pos) {
       auto  adjusted_NNUE = [&]()
       {
          int material = pos.nnue_material();
-         int scale =  505
+         int scale =  594
                     + material / 32
                     - 4 * pos.rule50_count();
 
