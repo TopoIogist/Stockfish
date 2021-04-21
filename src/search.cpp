e3d594d3aeb8137ec1bf288623d05f14d6b50be6
Topologist <thelisting@web.de>
2021-04-21 15:49:47 +0200

    Take 13 bench 4181322

--- src/search.cpp
+++ src/search.cpp
@@ -467,7 +467,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += ( delta * (215 + 26*std::clamp(rootDepth-6, Depth(0), Depth(8))) + 5200)/1024;
+              delta += ( delta * (222 + 25*std::clamp(rootDepth-5, Depth(0), Depth(8))) + 5200)/1024;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
