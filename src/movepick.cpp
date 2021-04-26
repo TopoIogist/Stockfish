81554919c95c6e14e375cb11aa027518fbf99b6d
Topologist <thelisting@web.de>
2021-04-26 21:32:01 +0200

    Take 7 bench 4550627

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -111,16 +111,12 @@ void MovePicker::score() {
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
-    {
-        m.value = (*mainHistory)[pos.side_to_move()][from_to(m)]
+        m.value = (*mainHistory)[pos.side_to_move()][from_to(m)] + PieceQuietValue[pos.moved_piece(m)]
                   + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                   + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                   + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                   + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
                   + (ply < MAX_LPH ? std::min(4, depth / 3) * (*lowPlyHistory)[ply][from_to(m)] : 0);
-        if((*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)] != 0)
-            m.value += PieceQuietValue[pos.moved_piece(m)];
-    }
 
       else // Type == EVASIONS
       {
