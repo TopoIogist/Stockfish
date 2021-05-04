156e0617722958bc94a971bfafe418b57ac669a4
Topologist <thelisting@web.de>
2021-05-05 00:19:19 +0200

    Take 2 bench 4035195

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1133,7 +1133,7 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Evaluation grain
-  v = (v / 5) * 5;
+  v = (v / 7) * 7;
 
   // Damp down the evaluation linearly when shuffling
   v = v * (100 - pos.rule50_count()) / 100;
