9734d0f8771f96ee5a0d1703f6a0f6b099b9f16c
Topologist <thelisting@web.de>
2021-05-05 16:01:13 +0200

    Take 6 bench 3814137

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1136,7 +1136,7 @@ Value Eval::evaluate(const Position& pos) {
   v = v * (100 - pos.rule50_count()) / 100;
 
   // Evaluation grain
-  v = Value(static_cast<int>(v) & 0xFFFFFFF8);
+  v = (v / 8) * 8;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
