69bda72a39280bda51cd58f5867cf0a80ef4559c
Topologist <thelisting@web.de>
2021-05-20 16:10:16 +0200

    Avoid bucket 0 bench 4171175

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1136,7 +1136,8 @@ Value Eval::evaluate(const Position& pos) {
 
       // Use classical evaluation for really low piece endgames.
       // One critical case is the draw for bishop + A/H file pawn vs naked king.
-      bool lowPieceEndgame =   pos.non_pawn_material() == BishopValueMg
+      const std::size_t bucket = (pos.count<ALL_PIECES>() - 1) / 4;
+      bool lowPieceEndgame =   pos.non_pawn_material() == BishopValueMg || (bucket == 0)
                             || (pos.non_pawn_material() < 2 * RookValueMg && pos.count<PAWN>() < 2);
 
       v = classical || lowPieceEndgame ? Evaluation<NO_TRACE>(pos).value()
